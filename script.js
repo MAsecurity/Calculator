8bc59acf78ccf670b408407e75851354e97727b6
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-08-03 11:08:26 +0100

    Extract all data attributes

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let currentOperandText = '';
 let previousOperandText = '';
 let operation = undefined;
 
+//Extract all relevant data attributes
+const dataPreviousOperand = document.querySelector("[data-previous-operand]");
+const dataCurrentOperand = document.querySelector("[data-current-operand]");
+const dataAllClear = document.querySelector("[data-all-clear]");
+const dataDelete = document.querySelector("[data-delete]");
+const dataEquals = document.querySelector("[data-equals]");
+const dataNumbers = document.querySelectorAll("data-number");
+const dataOperations = document.querySelectorAll("[data-operation]");
+
+
+
+
+
+
