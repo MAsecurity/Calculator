63c026e97ea6d19cd467c5a5528c7abba41bd8f9
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-08-03 11:44:39 +0100

    Update script.js -Loop over all numbers and add event listeners. -Create a number function which takes the number and modify  the currentoperandtext to include the number. -Prevent the decimal from being entered more than once. -Update the records using an update display function that should  be at the very bottom.

--- script.js
+++ script.js
@@ -9,8 +9,49 @@ const dataCurrentOperand = document.querySelector("[data-current-operand]");
 const dataAllClear = document.querySelector("[data-all-clear]");
 const dataDelete = document.querySelector("[data-delete]");
 const dataEquals = document.querySelector("[data-equals]");
-const dataNumbers = document.querySelectorAll("data-number");
+const dataNumbers = document.querySelectorAll("[data-number]");
 const dataOperations = document.querySelectorAll("[data-operation]");
+
+//Clear all records by default
+function clear() {
+  dataCurrentOperand.textContent = currentOperandText;
+  dataPreviousOperand.textContent = previousOperandText;
+}
+
+clear();
+
+//Loop over all numbers and add event listeners
+//Create a number function which takes the number and modifes the currentoperandtext to include the number
+//Then update the records using an update display function that should be at the very bottom.
+dataNumbers.forEach(numberBtn => {
+  numberBtn.addEventListener("click",function () {
+    number(numberBtn.textContent);
+    updateDisplay();
+    
+
+  })
+})
+
+function number(num) {
+  if(num === "."  && currentOperandText.includes(".")){
+    return;
+  }
+  currentOperandText = currentOperandText.toString() + num.toString();
+
+
+}
+
+
+
+function updateDisplay() {
+  dataCurrentOperand.textContent = currentOperandText;
+  dataPreviousOperand.textContent = previousOperandText;
+}
+
+
+
+
+
 
 
 
